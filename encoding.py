115002542863bb0b543100b59512ea4f5c095a90
Farah <kacem.farah@insat.u-carthage.tn>
2021-01-14 20:44:45 +0100

    i don't understand encoding help

--- encoding.py
+++ encoding.py
@@ -2,28 +2,28 @@ from base64 import b64encode
 from base64 import b64decode
 
 def encode():
-    print("State the encoding type: (exp: base64 )")
+    print("State the encoding type: (exp: utf-8, ascii )")
     entype = input()
-    print("State the message you would like to encode:") 
+    print("State the string you would like to encode:") 
     message = input()
     encodeTypes=["utf-8", "ascii"]
     if(entype in encodeTypes ):
-        print("Your encoded message is:")  
-        print(message.encode(entype))
-    if(entype == "base64"):
-        print("Your encoded message is:")  
-        print(b64encode(message.encode()))       
+        print("Your encoded string is:")  
+        print(message.encode(entype).decode())
+    # if(entype == "base64"):
+    #     print("Your encoded message is:")  
+    #     print(b64encode(message.encode()))       
 
 
 def decode():
-    print("State the encoding type: (exp: base64 )")
+    print("State the encoding type: (exp: ascii, utf-8 )")
     entype = input()
     print("State the message you would like to decode:") 
-    message = input()
+    message = input().encode(entype)
     encodeTypes=["utf-8", "ascii"]
     if(entype in encodeTypes ):
         print("Your decoded message is:")  
         print(message.decode(entype))
-    if(entype == "base64"):
-        print("Your decoded message is:")  
-        print(b64decode(message.encode()).decode('utf-8'))    
+    # if(entype == "base64"):
+    #     print("Your decoded message is:")  
+    #     print(b64decode(message).decode("utf-8", 'ignore'))
