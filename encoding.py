c1dcab04b3d4e538993addcf2036fe853a691904
Farah <kacem.farah@insat.u-carthage.tn>
2021-01-14 20:58:33 +0100

    ehh

--- encoding.py
+++ encoding.py
@@ -7,9 +7,9 @@ def encode():
     print("State the string you would like to encode:") 
     message = input()
     encodeTypes=["utf-8", "ascii"]
-    if(entype in encodeTypes ):
-        print("Your encoded string is:")  
-        print(message.encode(entype).decode())
+    # if(entype in encodeTypes ):
+    print("Your encoded string is:")  
+    print(message.encode(entype).decode())
     # if(entype == "base64"):
     #     print("Your encoded message is:")  
     #     print(b64encode(message.encode()))       
@@ -21,9 +21,9 @@ def decode():
     print("State the message you would like to decode:") 
     message = input().encode(entype)
     encodeTypes=["utf-8", "ascii"]
-    if(entype in encodeTypes ):
-        print("Your decoded message is:")  
-        print(message.decode(entype))
+    # if(entype in encodeTypes ):
+    print("Your decoded message is:")  
+    print(message.decode(entype))
     # if(entype == "base64"):
     #     print("Your decoded message is:")  
     #     print(b64decode(message).decode("utf-8", 'ignore'))
